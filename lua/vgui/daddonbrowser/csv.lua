6d6a0261df4d5451d731bebf0edefafb41d52fa1
WLKRE <wlkrstudios@hotmail.com>
2026-04-09 16:04:16 -0400

    Added CSV, removed RAW

--- lua/vgui/daddonbrowser/csv.lua
+++ lua/vgui/daddonbrowser/csv.lua
@@ -1,22 +1,26 @@
 
+-- passthrough
+local lua = include("lua.lua")
+
 local EXT = {}
 
 EXT.Icon = "icon16/page_white_excel.png"
 
 EXT.Initialize = function(browser, name, path, dir)
-	-- override
+	lua.Initialize(browser, name, path, dir)
 end
 
 EXT.Browse = function(browser, name, path, dir)
-	-- override
+	local code = file.Read(dir .. "/" .. name, "GAME")
+	lua.Browse(browser, name, path, dir, code, "csv")
 end
 
 EXT.RightClick = function(menu, name, path, dir)
-	-- override
+	lua.RightClick(menu, name, path, dir)
 end
 
 EXT.Invalidate = function()
-	-- override
+	lua.Invalidate()
 end
 
 return EXT
--- lua/vgui/daddonbrowser/raw.lua
+++ lua/vgui/daddonbrowser/raw.lua
@@ -1,25 +1,22 @@
 
---passthrough
-local vmt = include("vmt.lua")
-
 local EXT = {}
 
 EXT.Icon = "icon16/page_white_camera.png"
 
 EXT.Initialize = function(browser, name, path, dir)
-	vmt.Initialize(browser, name, path, dir)
+	-- override
 end
 
 EXT.Browse = function(browser, name, path, dir)
-	vmt.Browse(browser, name, path, dir)
+	-- override
 end
 
 EXT.RightClick = function(menu, name, path, dir)
-	vmt.RightClick(menu, name, path, dir)
+	-- override
 end
 
 EXT.Invalidate = function()
-	vmt.Invalidate()
+	-- override
 end
 
 return EXT
